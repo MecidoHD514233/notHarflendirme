e1215a6b951184a38c056b68e8e23f76357571a5
Abdulmecit ÖZTÜRK <147340064+MecidoHD514233@users.noreply.github.com>
2024-10-23 14:31:29 +0300

    notHarflendirme
    girilen notlara göre hangi harf notunu alacağınızı gösteriri

---
+++ Notproje/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notproje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen 1. sayıyı giriniz : ");
+            int birnot = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Lütfen 2. sayıyı giriniz : ");
+            int ikinot = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Lütfen 3. sayıyı giriniz : ");
+            int ucnot = Convert.ToInt32(Console.ReadLine());
+
+            int sonuc = (birnot + ikinot + ucnot) / 3;
+
+            if (sonuc >= 80 && sonuc<100)
+            {
+                Console.WriteLine(" Harf Notunuz A+,Geçtiniz Tebrikler.");
+            }
+            else if (60 <= sonuc && sonuc< 80)
+            {
+                Console.WriteLine("Harf Notunuz A,Geçtiniz Tebrikler.");
+            }
+            else if (40 <= sonuc && sonuc < 60)
+            {
+                Console.WriteLine(" Harf Notunuz B+ ,Geçtiniz Tebrikler.");
+            }
+            else
+            {
+                Console.WriteLine("Harf Notunuz F, Üzgünüz Maalesef Kaldınız.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
